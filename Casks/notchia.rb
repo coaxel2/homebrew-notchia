6978830a1a19efd8d779ae614da8143b84d94667
Axel Courty <axel.courty@orange.fr>
2026-09-25 11:21:10 +0200

    notchia 2.9.24

--- Casks/notchia.rb
+++ Casks/notchia.rb
@@ -1,6 +1,6 @@
 cask "notchia" do
-  version "2.9.23"
-  sha256 "ee8e1a9f8075fcc9b14a1195aba01796ec6cf3a17db7a24d3ffb734da0b43ef6"
+  version "2.9.24"
+  sha256 "deb38526dbcb0d8766d4c642f2e186594a3000009acfbbf163c9d82251a66350"
 
   url "https://github.com/coaxel2/NotchIA/releases/download/v#{version}/NotchIA.dmg"
   name "NotchIA"
